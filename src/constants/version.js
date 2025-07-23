3cfe5048962a4c428842c0bf28ca6da7e176393f
Leo Liu <lovexinlingfeiwu@foxmail.com>
2025-07-23 14:50:42 +0800

    chore: release v1.1.1

--- src/constants/version.js
+++ src/constants/version.js
@@ -1,29 +1,29 @@
 // 应用版本信息常量 (自动生成，请勿手动修改)
-// 生成时间: 2025-07-23T06:47:18.3NZ
+// 生成时间: 2025-07-23T06:50:39.3NZ
 
-export const APP_VERSION = '1.1.0';
-export const BUILD_TIME = '2025-07-23T06:47:18.3NZ';
-export const BUILD_HASH = 'ea4a04b902da';
-export const BUILD_TIMESTAMP = 1753253238;
+export const APP_VERSION = '1.1.1';
+export const BUILD_TIME = '2025-07-23T06:50:39.3NZ';
+export const BUILD_HASH = 'd2a2fb7dcc34';
+export const BUILD_TIMESTAMP = 1753253439;
 
 export const GIT_INFO = {
-  hash: '01306cc',
+  hash: '01857fe',
   branch: 'main',
   tag: ''
 };
 
 export const VERSION_INFO = {
   name: 'react-todo-app',
-  version: '1.1.0',
-  buildTime: '2025-07-23T06:47:18.3NZ',
-  buildHash: 'ea4a04b902da',
-  buildTimestamp: 1753253238,
+  version: '1.1.1',
+  buildTime: '2025-07-23T06:50:39.3NZ',
+  buildHash: 'd2a2fb7dcc34',
+  buildTimestamp: 1753253439,
   git: GIT_INFO
 };
 
 // 设置全局变量供应用使用
 if (typeof window !== 'undefined') {
-  window.__APP_VERSION__ = '1.1.0';
-  window.__BUILD_HASH__ = 'ea4a04b902da';
-  window.__BUILD_TIME__ = '2025-07-23T06:47:18.3NZ';
+  window.__APP_VERSION__ = '1.1.1';
+  window.__BUILD_HASH__ = 'd2a2fb7dcc34';
+  window.__BUILD_TIME__ = '2025-07-23T06:50:39.3NZ';
 }
